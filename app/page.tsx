52aaa1de206c55ea464e1a970120136d54accd18
Chaitanya Nevse <chaitanyanevse3012@gmail.com>
2025-12-31 15:15:57 +0530

    feat: implement new UI components including Combobox, DropdownMenu, Field, InputGroup, Input, Label, Select, Separator, Textarea, and utility functions for class name management

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Hello</div>;
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div className="p-8 justify-center items-center flex h-screen w-full">
+      <Button size="lg">Example</Button>
+    </div>
+  );
 }
