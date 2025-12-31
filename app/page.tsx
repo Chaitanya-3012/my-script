64e49b8610a01e64d85dc5619b896058e1a90116
Chaitanya Nevse <chaitanyanevse3012@gmail.com>
2025-12-31 16:44:36 +0530

    refactor: remove greet function integration from Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import greet from "@/service/script";
 export default function Page() {
   return (
     <div className="p-8 justify-center items-center flex h-screen w-full">
-      <Button size="lg" onClick={() => console.log(greet("User"))}>
-        Example
-      </Button>
+      <Button size="lg">Example</Button>
     </div>
   );
 }
