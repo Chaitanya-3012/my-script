622f16a5ef40563d74e0ddc443f8631b83c3766d
Chaitanya Nevse <chaitanyanevse3012@gmail.com>
2025-12-31 15:19:05 +0530

    feat: add greet function and integrate it with the Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { Button } from "@/components/ui/button";
-
+import greet from "@/service/script";
 export default function Page() {
   return (
     <div className="p-8 justify-center items-center flex h-screen w-full">
-      <Button size="lg">Example</Button>
+      <Button size="lg" onClick={() => console.log(greet("User"))}>
+        Example
+      </Button>
     </div>
   );
 }
